c855de5280aca3929225fc3ce1d1990773f56d64
Swapnil Sen <swapnil@asconsoftech.com>
2023-11-18 01:57:15 +0530

    Base Code Setup

---
+++ src/main/java/com/themindfulmountains/business/api/BusinessController.java
@@ -0,0 +1,13 @@
+package com.themindfulmountains.business.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BusinessController {
+    //API Specs
+    /*
+        1. Add new business
+        2. Get business by state/country/ location
+        3. Update a business
+     */
+}
---
+++ src/main/java/com/themindfulmountains/business/api/TransportController.java
@@ -0,0 +1,7 @@
+package com.themindfulmountains.business.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TransportController {
+}
