f9f56263f7210350ad6536fd2d89deb5bf27ee37
Swapnil Sen <swapnil@asconsoftech.com>
2023-11-18 02:11:41 +0530

    Update to deploy in Heckaro

--- src/main/java/com/themindfulmountains/business/api/BusinessController.java
+++ src/main/java/com/themindfulmountains/business/api/BusinessController.java
@@ -1,6 +1,8 @@
 package com.themindfulmountains.business.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BusinessController {
@@ -10,4 +12,10 @@ public class BusinessController {
         2. Get business by state/country/ location
         3. Update a business
      */
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 }
