8cb9eeab170dd1dc1863781246749aae66ed8b9b
Swapnil Sen <swapnilsen96@gmail.com>
2026-01-15 17:13:34 +0530

    Commit Config

--- src/main/java/com/themindfulmountains/business/config/GcsConfig.java
+++ src/main/java/com/themindfulmountains/business/config/GcsConfig.java
@@ -1,25 +1,19 @@
-/*
 package com.themindfulmountains.business.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.google.cloud.storage.Storage;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
     @Bean
     public Storage storage() throws IOException {
-        InputStream keyFile = new ClassPathResource("gcp-key.json").getInputStream();
-        return StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(keyFile))
-                .build()
-                .getService();
+        // This automatically finds credentials in the environment (ADC)
+        // Locally: It looks for GOOGLE_APPLICATION_CREDENTIALS env var
+        // On Cloud Run: It uses the built-in Service Account
+        return StorageOptions.getDefaultInstance().getService();
     }
 }
-*/
