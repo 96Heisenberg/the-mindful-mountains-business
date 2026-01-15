428e8407aa44c69d376ee0a597e1067c60465b63
Swapnil Sen <swapnilsen96@gmail.com>
2026-01-15 17:09:24 +0530

    Revert "SA Path Update"
    This reverts commit aa2ab258ee8d7b96a5b78fcbe569311e59229560.

--- src/main/java/com/themindfulmountains/business/config/GcsConfig.java
+++ src/main/java/com/themindfulmountains/business/config/GcsConfig.java
@@ -1,8 +1,8 @@
+/*
 package com.themindfulmountains.business.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.google.cloud.storage.Storage;
@@ -13,17 +13,13 @@ import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
-
-    @Value("${spring.cloud.gcp.storage.credentials.location}")
-    private String gcpKey;
-
     @Bean
     public Storage storage() throws IOException {
-        InputStream keyFile = new ClassPathResource(gcpKey).getInputStream();
+        InputStream keyFile = new ClassPathResource("gcp-key.json").getInputStream();
         return StorageOptions.newBuilder()
                 .setCredentials(GoogleCredentials.fromStream(keyFile))
                 .build()
                 .getService();
     }
 }
-
+*/
