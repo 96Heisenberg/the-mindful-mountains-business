680872b42f511e8ee2a3fe59c56623b3b557db8e
Swapnil Sen <swapnilsen96@gmail.com>
2026-01-17 01:50:36 +0530

    Update Property.java

--- src/main/java/com/themindfulmountains/business/model/Property.java
+++ src/main/java/com/themindfulmountains/business/model/Property.java
@@ -11,6 +11,7 @@ import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -42,7 +43,7 @@ public class Property {
     @Type(type = "jsonb") // Enable JSONB storage
     @Column(name = "image_urls", columnDefinition = "jsonb")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private List<String> imageUrls;
+    private List<String> imageUrls = new ArrayList<>();
 
     @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Room> rooms;
